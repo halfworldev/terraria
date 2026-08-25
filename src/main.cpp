d20a5e9fd5e4f1a4ddd1fef168f54e921a7baa04
halfworldev <AN32_AN32@proton.me>
2026-08-25 08:21:57 +0530

    added imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,91 @@
 #include <iostream>
+#include <raylib.h>
+#include <imgui.h>
+#include <rlImGui.h>
 
 int main()
 {
-    std::cout << "Hello" << std::endl;
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(800, 540, "Terraria");
+
+#pragma region imgui
+    rlImGuiSetup(true);
+
+    ImGuiIO &io = ImGui::GetIO();
+    // io.ConfigFlags |=
+    //     ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    // io.ConfigFlags |=
+    //     ImGuiConfigFlags_NavEnableGamepad;            // Enable gamepad
+    //     Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable; // Enable Docking
+    io.FontGlobalScale = 1;
+
+    // Themes
+    // ImGui::StyleColorsDark();
+    // ImGui::StyleColorsLight();
+    ImGui::StyleColorsClassic();
+#pragma endregion
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+#pragma region imgui
+        rlImGuiBegin();
+
+        ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+        ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+        ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+        ImGui::PopStyleColor(2);
+#pragma endregion
+
+        DrawText("Hello, World!", 190, 200, 20, BLACK);
+
+#pragma region imgui windows
+        ImGui::Begin("test");
+        ImGui::Text("Speed");
+        ImGui::SameLine();
+        ImGui::TextDisabled("(?)");
+
+        if (ImGui::IsItemHovered())
+        {
+            ImGui::BeginTooltip();
+            ImGui::Text("Controls how fast the player moves.");
+            ImGui::EndTooltip();
+        }
+
+        ImGui::End();
+
+        ImGui::Begin("Second window");
+
+        static char buffer[128] = "";
+
+        ImGuiInputTextFlags flags =
+            ImGuiInputTextFlags_CharsNoBlank |     // no spaces
+            ImGuiInputTextFlags_EnterReturnsTrue | // return true on Enter
+            ImGuiInputTextFlags_AutoSelectAll; // select all text when focused
+
+        if (ImGui::InputText("Player Name", buffer, sizeof(buffer), flags))
+        {
+            std::cout << "Confirmed name: " << buffer << "\n";
+        }
+
+        ImGui::End();
+#pragma endregion
+
+#pragma region imgui
+        rlImGuiEnd();
+#pragma endregion
+
+        EndDrawing();
+    }
+
+#pragma region imgui
+    rlImGuiShutdown();
+#pragma endregion
+
+    CloseWindow();
+
     return 0;
 };
